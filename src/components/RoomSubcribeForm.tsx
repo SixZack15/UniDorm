5f3e8564ff0189cd6a4f8245eeb284df53ca85fa
Cy4nXDreamworld <Hn971596@gmail.com>
2025-11-25 16:20:32 +0700

    feature: added input for CMND

--- src/components/RoomSubcribeForm.tsx
+++ src/components/RoomSubcribeForm.tsx
@@ -10,9 +10,11 @@ interface FormData {
   name: string;
   birthdate: string;
   phoneNumber: string;
+  cmnd: string;
   healthCheckupImage: File | null;
   parentName: string;
   parentPhoneNumber: string;
+  parentCmnd: string;
   roomType: string;
 }
 
@@ -23,9 +25,11 @@ const RoomSubscribeForm: React.FC = () => {
     name: '',
     birthdate: '',
     phoneNumber: '',
+    cmnd: '',
     healthCheckupImage: null,
     parentName: '',
     parentPhoneNumber: '',
+    parentCmnd: '',
     roomType: '',
   });
 
@@ -221,6 +225,13 @@ const RoomSubscribeForm: React.FC = () => {
           type="tel"
           placeholder="090 123 4567"
         />
+        <InputField 
+          icon={<svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" /></svg>}
+          label="CMND/CCCD"
+          name="cmnd"
+          type="text"
+          placeholder="079123456789"
+        />
 
         <h3 className="text-xl font-semibold text-red-600 border-b pb-2 mb-4 pt-4">Tài liệu Bổ sung</h3>
         <InputField 
@@ -246,6 +257,13 @@ const RoomSubscribeForm: React.FC = () => {
           name="parentPhoneNumber"
           type="tel"
           placeholder="098 765 4321"
+        />
+        <InputField 
+          icon={<svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" /></svg>}
+          label="CMND/CCCD Phụ huynh/Người giám hộ"
+          name="parentCmnd"
+          type="text"
+          placeholder="079987654321"
         />
 
         <button
